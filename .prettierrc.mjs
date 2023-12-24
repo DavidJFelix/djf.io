8f468915cf7d59ba24d16f366235e0944d3f3bd9
David J. Felix <felix.davidj@gmail.com>
2023-12-23 20:56:07 -0600

    feat: add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSpacing: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
