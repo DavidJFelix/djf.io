1c899e29097ed0d1d46fd3832e23b4481e6ad4dd
David J. Felix <felix.davidj@gmail.com>
2023-12-26 16:28:00 -0600

    feat: clear out old demo docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,14 @@ import {defineConfig} from 'astro/config'
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'DJF.io',
       social: {
         github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {label: 'Example Guide', link: '/guides/example/'},
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: {directory: 'reference'},
+          label: 'Blog',
+          autogenerate: {directory: 'blog'},
         },
       ],
     }),
